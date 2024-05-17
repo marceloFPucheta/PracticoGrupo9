74479894bb278014e8d42d0ef92adc68401cb425
stkener <stkener@gmail.com>
2024-05-17 16:04:23 -0300

    fotter e index responsive

--- static/js/inicio.js
+++ static/js/inicio.js
@@ -48,4 +48,17 @@ function fotoSiguiente(){
     resetearIntervalo()
     moverCarrusel()
 }
+
+const tarjetas = document.querySelectorAll('.tarjetaPromo a');
+
+    tarjetas.forEach(tarjeta => {
+        tarjeta.addEventListener('click', function(e) {
+            e.preventDefault(); // Evita que el enlace navegue a otra página
+            const titulo = this.querySelector('.tituloTarjeta').textContent;
+            alert(`Compraste la promo con destino a: ${titulo}`);
+        });
+    });
+
+
+
 });
